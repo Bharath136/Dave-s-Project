db6ff3262aa171fd96224d5e435aabf617c86ee2
Kiran kurva <kiran.k@fusiontechpartners.com>
2023-02-10 16:52:24 +0530

    Dave's Project Login/Sign Up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Login from './components/Login/login';
+import SignIn from './components/SignIn/signin';
+import LoginPhone from './components/LoginPhone/loginphone';
+import LoginOTP from './components/LoginOTP/loginotp';
+import ForgotPassword from './components/ForgotPassword/forgotpassword';
+
+import './App.css';
+import ResetPassword from './components/ResetPassword/resetpassword';
+
+const  App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signin" element={<SignIn/>}/>
+            <Route path='/forgotpassword' element={<ForgotPassword />} />
+            <Route path='/loginphone' element={<LoginPhone />} />
+            <Route path="resetpassword" element={<ResetPassword />} />
+            <Route path='/loginotp' element={<LoginOTP />}/>
+            
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
